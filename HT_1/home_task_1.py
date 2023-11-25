d0fca297dbec1f36e8a68db323778625f3914c22
Egor Makushkin <makushkin99@mail.com>
2023-11-25 13:06:26 +0300

    Test

--- HT_1/home_task_1.py
+++ HT_1/home_task_1.py
@@ -1,7 +1,14 @@
-class Student:
-    def __init__(self, name, surname, sex):
+class SchoolMember:
+
+    def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+
+
+class Student(SchoolMember):
+
+    def __init__(self, name, surname, sex):
+        super().__init__(name, surname)
         self.sex = sex
         self.courses_finished = []
         self.grades = {}
@@ -10,12 +17,31 @@ class Student:
     def add_courses_finished(self, course_finished_name):
         self.courses_finished.append(course_finished_name)
 
+    def add_lucturer_grade(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course not in lecturer.grades:
+                lecturer.grades[course] = [grade]
+            else:
+                lecturer.grades[course].append(grade)
+        else:
+            return 'Ошибка'
 
-class Teacher:
+
+class Teacher(SchoolMember):
+
     def __init__(self, name, surname):
-        self.name = name
-        self.surname = surname
+        super().__init__(name, surname)
         self.courses_attached = []
+
+
+class Lecturer(Teacher):
+
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+
+class Reviewer(Teacher):
 
     def add_student_grade(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -26,12 +52,6 @@ class Teacher:
         else:
             return 'Ошибка'
 
-class Lecturer(Teacher):
-    pass
-
-class Reviewer(Teacher):
-    pass
-
 
 student_1 = Student('Ivan', 'Ivanov', 'male')
 student_1.courses_finished.append('Git')
@@ -39,12 +59,24 @@ student_1.courses_in_progress.append('Python')
 student_1.grades['Git'] = [10, 10, 10, 10, 10]
 student_1.grades['Python'] = [10, 10]
 
-teacher_1 = Teacher('Tom', 'Tailor')
-teacher_1.courses_attached.append('Python')
+reviewer_1 = Reviewer('Viktor', 'Stepanov')
+reviewer_1.courses_attached.append('Python')
+reviewer_1.courses_attached.append('Git')
 
-teacher_1.add_student_grade(student_1, 'Python', 10)
-teacher_1.add_student_grade(student_1, 'Python', 10)
-teacher_1.add_student_grade(student_1, 'Python', 10)
+lecturer_1 = Lecturer('Tom', 'Tailor')
+lecturer_1.courses_attached.append('Python')
+lecturer_1.courses_attached.append('Git')
+lecturer_1.grades['Git'] = [5, 5, 4, 7]
+lecturer_1.grades['Python'] = [3, 10, 8]
+
+reviewer_1.add_student_grade(student_1, 'Python', 7)
+reviewer_1.add_student_grade(student_1, 'Python', 8)
+reviewer_1.add_student_grade(student_1, 'Python', 6)
+
+student_1.add_lucturer_grade(lecturer_1, 'Python', 5)
+student_1.add_lucturer_grade(lecturer_1, 'Python', 10)
+student_1.add_lucturer_grade(lecturer_1, 'Python', 10)
 
 print(student_1.grades)
+print(lecturer_1.grades)
 
